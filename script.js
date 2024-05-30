0eff012c3bd8f8a945c08c6c736f61b2bc8f528c
octaviareika <ceciliaperet81@gmail.com>
2024-05-29 21:15:27 -0300

    iniciando

--- script.js
+++ script.js
@@ -2,4 +2,31 @@ import getDados from "./js/dados";
 
 var datasSecao = { 
     tudo: document.querySelector('[data-name=todos]'),
+    recente: document.querySelector('[data-name=recentes]'),
+    economico: document.querySelector('[data-name=economicos]'),
+}
+
+function criaListaVeiculos(elemento, data){
+    const ulExistente = elemento.querySelector('ul'); // Verifica se já existe uma lista de veículos
+
+    if(ulExistente){
+        elemento.removeChild(ulExistente); // Se existir, remove a lista
+    }
+
+    else {
+        const ulNova = document.createElement('ul'); // Se não existir, cria uma nova lista
+        ulNova.classList.add('lista-veiculos'); // Adiciona a classe 'lista-veiculos' à nova lista
+        const listaHTML = 
+                    data.map(veiculo => `
+                        <li class="veiculo">
+                            <img src="${veiculo.imagem}" alt="${veiculo.nome}">
+                            <h2>${veiculo.nome}</h2>
+                            <p>${veiculo.descricao}</p>
+                            <p>R$ ${veiculo.preco}</p>
+                        </li>
+                    `).join('');
+        
+                    ulNova.innerHTML = listaHTML; // Adiciona o conteúdo da lista
+                    elemento.appendChild(ulNova); // Adiciona a lista ao elemento
+    }
 }
