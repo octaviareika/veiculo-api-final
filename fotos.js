26b87d9cc88d62d35384f4554ae25f5a81c24568
Cecilia <ceciliaperet81@gmail.com>
2024-06-04 21:05:13 -0300

    projeto quase final

---
+++ fotos.js
@@ -0,0 +1,77 @@
+const carouselSlide = document.querySelector('.carrosel-imagem');
+const carouselImages = document.querySelectorAll('.carrosel-imagem img');
+
+const prevBtn = document.getElementById('prevBtn');
+const nextBtn = document.getElementById('nextBtn');
+
+class Slide {
+    constructor(carouselSlide, carouselImages, prevBtn, nextBtn) {
+        this.carouselSlide = carouselSlide;
+        this.carouselImages = carouselImages;
+        this.prevBtn = prevBtn;
+        this.nextBtn = nextBtn;
+        this.counter = 0; // contador para saber em qual imagem estamos
+        this.size = this.carouselImages[0].clientWidth; // pega o tamanho da imagem
+        this.carouselSlide.style.transform = 'translateX(' + (-this.size * this.counter) + 'px)'; // inicializa o slide no primeiro item
+
+        // Clone the first and last image
+        let firstClone = this.carouselImages[0].cloneNode(true);
+        let lastClone = this.carouselImages[this.carouselImages.length - 1].cloneNode(true);
+
+        // Add the clones to the carousel
+        this.carouselSlide.append(firstClone); // adiciona o clone no final
+        this.carouselSlide.prepend(lastClone); // adiciona o clone no inicio
+    }
+
+    start() {
+        setInterval(() => {
+           
+            this.next();
+        }, 1000); // muda paara a proxima imagem a cada 1 segundo
+    }
+
+    next() {
+        if (this.counter >= this.carouselImages.length - 5) { // se for a ultima imagem
+            this.direction = -1;
+        } else if (this.counter <= 0) {
+            this.direction = 1;
+        }
+        this.carouselSlide.style.transition = "transform 0.4s ease-in-out";
+        this.counter += this.direction;
+        this.carouselSlide.style.transform = 'translateX(' + (-this.size * this.counter) + 'px)';
+    }
+    
+    prev() {
+        if (this.counter <= 0) {
+            return;
+        }
+        this.carouselSlide.style.transition = "transform 0.4s ease-in-out";
+        this.counter--;
+        this.carouselSlide.style.transform = 'translateX(' + (-this.size * this.counter) + 'px)';
+    }
+
+    setupEventListeners() {
+        this.carouselSlide.addEventListener('transitionend', () => {
+            if (this.carouselImages[this.counter].id === 'lastClone') {
+                this.carouselSlide.style.transition = "none";
+                this.counter = this.carouselImages.length - 2;
+                this.carouselSlide.style.transform = 'translateX(' + (-this.size * this.counter) + 'px)';
+            }
+            if (this.carouselImages[this.counter].id === 'firstClone') {
+                this.carouselSlide.style.transition = "none";
+                this.counter = this.carouselImages.length - this.counter;
+                this.carouselSlide.style.transform = 'translateX(' + (-this.size * this.counter) + 'px)';
+            }
+        });
+    }
+}
+
+
+// criar um new Slide()
+
+let slide = new Slide(carouselSlide, carouselImages, prevBtn, nextBtn);
+
+if (slide) {
+    slide.start();
+    slide.setupEventListeners();
+}
--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-import getDados from "./js/dados";
+var url = 'http://localhost:8080'; // URL do servidor
+
+export default function getDados(rotas) {
+    return fetch(`${url}${rotas}`)
+        .then(response => response.json())
+        .catch(error => {
+            console.error('Erro ao acessar a rota' + error);
+        })
+
+
+}
 
 var datasSecao = { 
     tudo: document.querySelector('[data-name=todos]'),
@@ -19,9 +29,9 @@ function criaListaVeiculos(elemento, data){
         const listaHTML = 
                     data.map(veiculo => `
                         <li class="veiculo">
-                            <h2>${veiculo.nome}</h2>
-                            <p>${veiculo.ano}</p>
-                            <p>R$ ${veiculo.valor}</p>
+                            <h2 class="h2-veiculo">${veiculo.modelo}</h2>
+                            <p class="p-veiculos paragrafo">${veiculo.marca}</p>
+                            <p class="p-veiculos valor">R$ ${veiculo.valor}</p>
                         </li>
                     `).join('');
         
@@ -32,27 +42,56 @@ function criaListaVeiculos(elemento, data){
 
 
 
-var luxo = document.querySelector('[data-name=luxo]');
-// display noone
-luxo.style.display = 'none';
-
-
 
 
 geraVeiculos();
 function geraVeiculos(){
-    var urls = ['/veiculos/todos', '/veiculos/recentes', '/veiculos/economicos'];
+    var urls = ['/veiculos', '/veiculos/recentes', '/veiculos/economicos'];
 
     try {
         Promise.all(urls.map(url => getDados(url)))
         .then(respostas => {
-            respostas.forEach((resposta, index) => {
-                criaListaVeiculos(datasSecao.tudo, resposta[0]);
-                criaListaVeiculos(datasSecao.recente, resposta[1]);
-                criaListaVeiculos(datasSecao.economico, resposta[2]);
+            respostas.forEach((resposta, indice) => {
+                criaListaVeiculos(datasSecao[Object.keys(datasSecao)[indice]], resposta);
+            });
+
+            // Limite de itens a serem exibidos inicialmente
+            const limite = 5;
+
+            // Seleciona a seção "todos"
+            const secaoTodos = document.querySelector('[data-name=todos]');
+
+            // Seleciona todos os itens na seção "todos"
+            const itensTodos = secaoTodos.querySelectorAll('.veiculo');
+
+            // Oculta todos os itens que estão acima do limite
+            for (let i = limite; i < itensTodos.length; i++) {
+                itensTodos[i].style.display = 'none';
+            }
+
+            const botaoVerMais = secaoTodos.querySelector('.btn-ver-mais');
+
+            botaoVerMais.addEventListener('click', () => {
+                
+                if (botaoVerMais.innerText === 'Ver mais') {
+                    for (let i = limite; i < itensTodos.length; i++) {
+                        itensTodos[i].style.display = 'block';
+                    }
+                    botaoVerMais.innerText = 'Ver menos';
+                } else {
+                    //oculta todos os itens que estão acima do limite
+                    for (let i = limite; i < itensTodos.length; i++) {
+                        itensTodos[i].style.display = 'none';
+                    }
+                    botaoVerMais.innerText = 'Ver mais';
+                }
+                
             });
         });
+
+       
     } catch (error) {
         console.log(error);
     }
 }
+
